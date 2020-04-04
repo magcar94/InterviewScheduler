e4cdd10c89551ad9975f21437a118f213e52d126
Magnus Berntsen Caro <magnus.caro@outlook.com>
2020-04-05 00:55:02 +0200

    Parsing of lines now works correctly, and added extracting of employers

--- IntSched/IntSched.cpp
+++ IntSched/IntSched.cpp
@@ -5,18 +5,15 @@
 #include <string>
 #include <vector>
 
-
-
-
 std::vector<std::string> ParseLine(std::string line)
 {
-	std::istringstream sline (line);
+	std::istringstream ss (line);
 	std::vector<std::string> cell;
 	char c;
-	std::string s;
-	while (sline >> s)
+	std::string token;
+	while (std::getline(ss, token, ','))
 	{
-		cell.push_back(s);
+		cell.push_back(token);
 	}
 
 	return cell;
@@ -38,18 +35,48 @@ std::vector<std::vector<std::string>> ReadInterviewList(std::string path)
 	return RetInterviewList;
 }
 
-void PrintList(std::vector<std::vector<std::string>> List)
+void Print2DStringList(std::vector<std::vector<std::string>> List)
 {
 	for (int i = 0; i < List.size(); i++) {
+		std:: cout << "[";
 		for (int j = 0; j < List[i].size(); j++) {
-			std::cout << List[i][j];
+			std::cout << "("<< List[i][j] <<")"<< " ";
 		}
-		std::cout << "\n";
+		std::cout << "]" << "\n";
 	}
 }
+void PrintStringList(std::vector<std::string> List)
+{
+	for (auto row : List) 
+	{
+		std::cout << "[";
+			std::cout << row << " ";
+		std::cout << "]" << "\n";
+	}
+}
+
+void FillEmployers(std::vector<std::vector<std::string>> InputList, std::vector < std::string > & EmployerList)
+{
+	for (int i = 0; i < InputList.size(); i++) {
+		std::cout << "[";
+		for (int j = 0; j < InputList[i].size(); j++) {
+			std::cout << "(" << InputList[i][j] << ")" << " ";
+
+			if (j == 0)
+			{
+				EmployerList.push_back(InputList[i][j]);
+			}
+		}
+		std::cout << "]" << "\n";
+	}
+}
+
 
 int main()
 {
 	auto IntervList = ReadInterviewList("D:/Git/InterviewScheduler/IntSched/InterviewList.txt");
-	PrintList(IntervList);
+	std::vector<std::string> EmployerList;
+	FillEmployers(IntervList, EmployerList);
+	Print2DStringList(IntervList);
+	PrintStringList(EmployerList);
 }
